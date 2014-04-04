b8e24bdd3f1f245267331ddb236d3bfaf316dee0
ayberkozgur <ayberk.ozgur@epfl.ch>
2014-04-04 16:50:17 +0200

    Isolated shaders into their own objects

--- samples-android/estimate3d-gui-opengl-canny/src/ch/epfl/chili/chilitags/samples/estimate3d_gui_opengl_canny/CameraPreviewRenderer.java
+++ samples-android/estimate3d-gui-opengl-canny/src/ch/epfl/chili/chilitags/samples/estimate3d_gui_opengl_canny/CameraPreviewRenderer.java
@@ -11,10 +11,11 @@ import javax.microedition.khronos.opengles.GL10;
 
 import ch.epfl.chili.chilitags.Chilitags3D;
 import ch.epfl.chili.chilitags.ObjectTransform;
+import ch.epfl.chili.chilitags.samples.estimate3d_gui_opengl_canny.shader.Shader;
+import ch.epfl.chili.chilitags.samples.estimate3d_gui_opengl_canny.shader.YUV2RGBShader;
 import android.graphics.Point;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.util.Log;
 
 public class CameraPreviewRenderer implements GLSurfaceView.Renderer {
 
@@ -39,53 +40,9 @@ public class CameraPreviewRenderer implements GLSurfaceView.Renderer {
 	//Our line object
 	private GLESLine line;
 
-	//Our vertex shader code; nothing special
-	private final String vertexShaderSource = 
-			"attribute vec4 a_position;							\n" + 
-			"attribute vec2 a_texCoord;							\n" + 
-			"varying vec2 v_texCoord;							\n" + 
-
-			"void main(){										\n" + 
-			"   gl_Position = a_position;						\n" + 
-			"   v_texCoord = a_texCoord;						\n" +
-			"}													\n";
-
-	//Our fragment shader code; takes Y,U,V values for each pixel and calculates R,G,B colors,
-	//Effectively making YUV to RGB conversion
-	private final String fragmentShaderSource = 
-			"#ifdef GL_ES										\n" +
-					"precision highp float;								\n" +
-					"#endif												\n" +
-
-			"varying vec2 v_texCoord;							\n" +
-			"varying vec2 v_texCoordChroma;						\n" +
-			"uniform sampler2D y_texture;						\n" +
-			"uniform sampler2D uv_texture;						\n" +
-
-			"void main (void){									\n" +
-			"	float r, g, b, y, u, v;							\n" +
-
-			//We had put the Y values of each pixel to the R,G,B components by GL_LUMINANCE, 
-			//that's why we're pulling it from the R component, we could also use G or B
-			"	y = texture2D(y_texture, v_texCoord).r;			\n" + 
-
-			//We had put the U and V values of each pixel to the A and R,G,B components of the
-			//texture respectively using GL_LUMINANCE_ALPHA. Since U,V bytes are interspread 
-			//in the texture, this is probably the fastest way to use them in the shader
-			"	u = texture2D(uv_texture, v_texCoord).a - 0.5;	\n" +									
-			"	v = texture2D(uv_texture, v_texCoord).r - 0.5;	\n" +
-
-			//The numbers are just YUV to RGB conversion constants
-			"	r = y + 1.13983*v;								\n" +
-			"	g = y - 0.39465*u - 0.58060*v;					\n" +
-			"	b = y + 2.03211*u;								\n" +
-
-			//We finally set the RGB color of our pixel
-			"	gl_FragColor = vec4(r, g, b, 1.0);				\n" +
-			"}													\n"; 
-
-	private int shaderProgramHandle; //The shader program object that will do the YUV-RGB conversion for us
-
+	//private int shaderProgramHandle; //The shader program object that will do the YUV-RGB conversion for us
+	private Shader yuv2rgbShader;
+	
 	private int positionHandle; //The location of the a_position attribute object
 	private int texCoordHandle; //The location of the a_texCoord attribute object
 
@@ -125,6 +82,9 @@ public class CameraPreviewRenderer implements GLSurfaceView.Renderer {
 		 * GLES stuff
 		 */
 
+		//Create our shaders
+		yuv2rgbShader = new YUV2RGBShader();
+		
 		//Allocate vertices of our mesh on native memory space
 		vertices = ByteBuffer.allocateDirect(verticesData.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
 		vertices.put(verticesData);
@@ -187,22 +147,23 @@ public class CameraPreviewRenderer implements GLSurfaceView.Renderer {
 		 * Prepare the shader stuff
 		 */
 
-		//Compile and load our shader
-		shaderProgramHandle = loadProgram(vertexShaderSource, fragmentShaderSource);
+		//Compile and load our shaders
+		yuv2rgbShader.load();
+		line.load();
 
 		//Get the attribute locations
-		positionHandle = GLES20.glGetAttribLocation(shaderProgramHandle, "a_position");
-		texCoordHandle = GLES20.glGetAttribLocation(shaderProgramHandle, "a_texCoord");
+		positionHandle = GLES20.glGetAttribLocation(yuv2rgbShader.getHandle(), "a_position");
+		texCoordHandle = GLES20.glGetAttribLocation(yuv2rgbShader.getHandle(), "a_texCoord");
 
 		//Create the Y texture object
 		GLES20.glEnable(GLES20.GL_TEXTURE_2D);
-		yTextureHandle = GLES20.glGetUniformLocation(shaderProgramHandle, "y_texture");
+		yTextureHandle = GLES20.glGetUniformLocation(yuv2rgbShader.getHandle(), "y_texture");
 		yTextureNames = new int[1];
 		GLES20.glGenTextures(1, yTextureNames, 0);
 
 		//Create the UV texture object
 		GLES20.glEnable(GLES20.GL_TEXTURE_2D);
-		uvTextureHandle = GLES20.glGetUniformLocation(shaderProgramHandle, "uv_texture");
+		uvTextureHandle = GLES20.glGetUniformLocation(yuv2rgbShader.getHandle(), "uv_texture");
 		uvTextureNames = new int[1];
 		GLES20.glGenTextures(1, uvTextureNames, 0);
 
@@ -257,7 +218,6 @@ public class CameraPreviewRenderer implements GLSurfaceView.Renderer {
 		//Get the camera image
 		byte[] cameraImage = camController.getPictureData();
 		if(cameraImage != null){
-
 
 			System.arraycopy(cameraImage, 0, buf, 0, 1280*720);
 			final int[] gaussian = {
@@ -400,7 +360,7 @@ public class CameraPreviewRenderer implements GLSurfaceView.Renderer {
 		final double[] WORLD_ARROW_Y = 		{0.0,			TAG_SIZE,	0.0,		1.0};
 		final double[] WORLD_ARROW_Z = 		{0.0,			0.0,		TAG_SIZE,	1.0};
 
-		line.begin();
+		
 
 		for(ObjectTransform tag : tags){
 
@@ -482,7 +442,7 @@ public class CameraPreviewRenderer implements GLSurfaceView.Renderer {
 		uvBuffer.position(0);
 
 		//Use the shader program object
-		GLES20.glUseProgram(shaderProgramHandle);
+		GLES20.glUseProgram(yuv2rgbShader.getHandle());
 
 		//Load the vertex position
 		vertices.position(0);
@@ -550,91 +510,5 @@ public class CameraPreviewRenderer implements GLSurfaceView.Renderer {
 		//Unload our vertex array
 		GLES20.glDisableVertexAttribArray(positionHandle);
 		GLES20.glDisableVertexAttribArray(texCoordHandle);
-	}
-
-	/**
-	 * Compiles the given shader code and returns its program handle.
-	 * 
-	 * @param type The type of the shader
-	 * @param source The GLSL source code of the shader
-	 * @return The program handle of the compiled shader
-	 */
-	public static int loadShader(int type, String source) {
-		int shader;
-		int[] compiled = new int[1];
-
-		//Create the shader object
-		shader = GLES20.glCreateShader(type);
-		if(shader == 0)
-			return 0;
-
-		//Load the shader source
-		GLES20.glShaderSource(shader, source);
-
-		//Compile the shader
-		GLES20.glCompileShader(shader);
-
-		//Check the compile status
-		GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
-
-		if(compiled[0] == 0){
-			Log.e("ESShader", GLES20.glGetShaderInfoLog(shader));
-			GLES20.glDeleteShader(shader);
-			return 0;
-		}
-		return shader;
-	}
-
-	/**
-	 * Compiles and links the individual shaders into a complete program and returns the program handle.
-	 * 
-	 * @param vertexShaderSource The vertex shader GLSL source code
-	 * @param fragmentShaderSource The fragment shader GLSL source code
-	 * @return The handle of the shader program
-	 */
-	public static int loadProgram(String vertexShaderSource, String fragmentShaderSource) {
-		int vertexShader;
-		int fragmentShader;
-		int programObject;
-		int[] linked = new int[1];
-
-		//Load the vertex/fragment shaders
-		vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderSource);
-		if(vertexShader == 0)
-			return 0;
-
-		fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderSource);
-		if(fragmentShader == 0){
-			GLES20.glDeleteShader(vertexShader);
-			return 0;
-		}
-
-		//Create the program object
-		programObject = GLES20.glCreateProgram();
-
-		if(programObject == 0)
-			return 0;
-
-		GLES20.glAttachShader(programObject, vertexShader);
-		GLES20.glAttachShader(programObject, fragmentShader);
-
-		//Link the program
-		GLES20.glLinkProgram(programObject);
-
-		//Check the link status
-		GLES20.glGetProgramiv(programObject, GLES20.GL_LINK_STATUS, linked, 0);
-
-		if(linked[0] == 0){
-			Log.e("ESShader", "Error linking program:");
-			Log.e("ESShader", GLES20.glGetProgramInfoLog(programObject));
-			GLES20.glDeleteProgram(programObject);
-			return 0;
-		}
-
-		//Free up no longer needed shader resources
-		GLES20.glDeleteShader(vertexShader);
-		GLES20.glDeleteShader(fragmentShader);
-
-		return programObject;
 	}
 }
